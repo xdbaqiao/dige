32a6f564f086671911d91f281ed93ab82685d4af
root <root@VM_0_15_centos.(none)>
2018-07-29 23:00:18 +0800

    add stat_data

---
+++ stat_data.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+import re
+from SQLConnector import MySQLConnect
+
+def stat_data():
+    # 统计当日、5日、20日等涨跌幅数据
+    conn = MySQLConnect('localhost', 'root', 'yexinjing', 'dige')
+    sql = '''
+    
+    
+    '''
+
+if __name__ == '__main__':
+    stat_data()
